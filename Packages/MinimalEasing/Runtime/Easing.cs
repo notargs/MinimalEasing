405c47ee6d5a0a9eae4637401c27d9abb32dac5f
notargs <satoyutaka94@gmail.com>
2023-12-16 16:00:09 +0900

    コンパイルエラーを修正

--- Packages/MinimalEasing/Runtime/Easing.cs
+++ Packages/MinimalEasing/Runtime/Easing.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using System.Threading;
 using Cysharp.Threading.Tasks;
+using Unity.Mathematics;
 using UnityEngine;
 
 namespace MinimalEasing
